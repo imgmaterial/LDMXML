60df93e55a8fd8437f58d38516241ea7d8bb3575
Udcstb99 <123908939+Udcstb99@users.noreply.github.com>
2023-06-26 16:46:05 -0400

    Update AuroraJobEx.sh

--- AuroraJobEx.sh
+++ AuroraJobEx.sh
@@ -5,4 +5,4 @@
 #SBATCH --mem-per-cpu=6000 #This tells the system how much memory should be used per CPU involved in the calculation of the job 
 #SBATCH -N 2 #This specifies how many nodes should be used for the job 
 
-python CNNModelANN.py #This is how you actually tell the system which python file to run so simply implement whichever file is to be run 
+python YourFileHere.py #This is how you actually tell the system which python file to run so simply implement whichever file is to be run 
