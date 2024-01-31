6b99454720c0db82116b3e76c19fdb27c604d516
Pavel Oshchepkov <paul.oshchepkov@icloud.com>
2024-01-31 12:23:01 +0100

    Abstracted RNN pre-processing into functions and fixed syntax errors

---
+++ RNN_Pre_Processing/RNN2e/RNNEcal2e.py
@@ -0,0 +1,61 @@
+import pandas as pd
+import numpy as np
+
+from libDetDescr import EcalID, HcalID,EcalTriggerID
+
+def RNN_Ecal_2e(ecal_path:str, energy_path:str,output_path:str, n:int = 1000, nc:int = 0):
+    readerC = pd.read_csv(ecal_path, chunksize=n)
+    for chunkC in readerC:
+        nE = 0
+        NRR = chunkC.shape[0]
+        NC = chunkC.shape[1]
+        readerE = pd.read_csv(energy_path, chunksize=n)
+        for chunkE in readerE:
+            #nTS = 0
+            if nE<nc:
+                nE = nE+1
+                continue
+            elif nE>nc:
+                break
+            elif nE==nc:
+                if nc == 0:
+                    for j in range(n):
+                        B = np.zeros(shape = (250))
+                        for i in range(250):#155 was the highest in all the events considered but if you're working with new data might have to change this number
+                            D = chunkC['{}'.format(i)][j]
+                            if pd.isna(D) == False and pd.isna(chunkE['{}'.format(i)][j])==False:
+                                DD = int(D)
+                                C = EcalID(DD).cell()
+                                M = EcalID(DD).module()
+                                L = EcalID(DD).layer()
+                                B[i] = (L)*10000000+1000000*(M)+1000*C+chunkE['{}'.format(i)][j]
+                        B = np.array(B)
+                        np.savez_compressed(output_path+'2e{}.npz'.format(nc), B)#Saves the array to a file named for example 1e1.npz in the NPZ file format.
+                elif NRR <n:
+                    for j in range(nc*n,nc*n+NRR):
+                                B = np.zeros(shape = (250))
+                                for i in range(250):
+                                        D = chunkC['{}'.format(i)][j]
+                                        if pd.isna(D) == False and pd.isna(chunkE['{}'.format(i)][j])==False:
+                                            DD = int(D)
+                                            C = EcalID(DD).cell()
+                                            M = EcalID(DD).module()
+                                            L = EcalID(DD).layer()
+                                            B[i] = (L)*10000000+1000000*(M)+1000*C+chunkE['{}'.format(i)][j]
+                                B = np.array(B)
+                                np.savez_compressed(output_path + '2e{}.npz'.format(nc), B)
+                else:
+                    for j in range(nc*NRR,(nc+1)*NRR):
+                                B = np.zeros(shape = (250))
+                                for i in range(250):
+                                        D = chunkC['{}'.format(i)][j]
+                                        if pd.isna(D) == False and pd.isna(chunkE['{}'.format(i)][j])==False:
+                                            DD = int(D)
+                                            C = EcalID(DD).cell()
+                                            M = EcalID(DD).module()
+                                            L = EcalID(DD).layer()
+                                            B[i] = (L)*10000000+1000000*(M)+1000*C+chunkE['{}'.format(i)][j]
+                                B = np.array(B)
+                                np.savez_compressed(output_path + '2e{}.npz'.format(nc), B)
+                nE = nE+1
+        nc = nc+1
---
+++ RNN_Pre_Processing/RNN3e/RNNETSX3e.py
@@ -0,0 +1,153 @@
+import pandas as pd
+import numpy as np
+from libDetDescr import EcalID, HcalID,EcalTriggerID
+
+def RNN_ETSX_3e(ecal_path:str, energy_path:str, tspos_path:str ,output_path:str, n:int = 1000, nc:int = 0):
+    readerC = pd.read_csv(ecal_path, chunksize=n) #This assumes that the file containing the EcalIDs is too large to load into RAM if not then remove the chunking of the csv
+    for chunkC in readerC:#Will iterate over the chunks of the read csv file
+        nE = 0
+        NRR = chunkC.shape[0]
+        NC = chunkC.shape[1]
+        readerE = pd.read_csv(energy_path, chunksize=n)#
+        for chunkE in readerE:
+            nTS = 0
+            if nE<nc:
+                nE = nE+1
+                continue
+            elif nE>nc:
+                break
+            elif nE==nc:
+                readerTS = pd.read_csv(tspos_path, chunksize=n)#Once again assuming that the TS file is too large
+                for chunkTS in readerTS:
+                    if nTS<nc:
+                        nTS = nTS+1
+                        continue
+                    elif nTS>nc:
+                        break
+                    elif nTS == nc and nTS == nE:
+                        if nc==0:
+                            for j in range(n):
+                                B = np.zeros((363))
+                                Y1 = int(chunkTS['{}'.format('Y1')][j])
+                                Y2 = int(chunkTS['{}'.format('Y2')][j])
+                                Y3 = int(chunkTS['{}'.format('Y3')][j])
+                                                
+                                X1 = int(chunkTS['{}'.format('X1')][j])
+                                X2 = int(chunkTS['{}'.format('X2')][j])
+                                X3 = int(chunkTS['{}'.format('X3')][j])
+                                                
+                                if Y1 == Y3 and Y1==Y2 and X1 == X3 and X1 == X2:
+                                    B[0] = (X1*1000000+Y1*1000+1)
+                                    B[1] = 0
+                                    B[2] = 0
+                                elif Y3==Y2 and X3==X2:
+                                    B[0] = (X1*1000000+Y1*1000+1)
+                                    B[1] = (X2*1000000+Y2*1000+1)
+                                    B[2] = 0
+                                elif Y1==Y2 and X1==X2:
+                                    B[0] = (X2*1000000+Y2*1000+1)
+                                    B[1] = 0
+                                    B[2] = (X3*1000000+Y3*1000+1)
+                                elif Y1==Y3 and X1==X3:
+                                    B[0] = (X1*1000000+Y1*1000+1)
+                                    B[1] = (X2*1000000+Y2*1000+1)
+                                    B[2] = 0
+                                else:
+                                    B[0] = (X1*1000000+Y1*1000+1)
+                                    B[1] = (X2*1000000+Y2*1000+1)
+                                    B[2] = (X3*1000000+Y3*1000+1)
+                                for i in range(360):
+                                        D = chunkC['{}'.format(i)][j]
+                                        if pd.isna(D) == False and pd.isna(chunkE['{}'.format(i)][j])==False:
+                                                DD = int(D)
+                                                C = EcalID(DD).cell()
+                                                M = EcalID(DD).module()
+                                                L = EcalID(DD).layer()
+                                                B[i+3] = (L+1)*10000000+1000000*(M)+1000*C+chunkE['{}'.format(i)][j]
+                                B = np.array(B)
+                                np.savez_compressed(output_path + '3e{}.npz'.format(nc), B)#Saves the array to a file named for example 1e1.npz in the NPZ file format.
+                        elif NRR <n:
+                            for j in range(nc*n,nc*n+NRR):
+                                B = np.zeros((363))
+                                Y1 = int(chunkTS['{}'.format('Y1')][j])
+                                Y2 = int(chunkTS['{}'.format('Y2')][j])
+                                Y3 = int(chunkTS['{}'.format('Y3')][j])
+                                                
+                                X1 = int(chunkTS['{}'.format('X1')][j])
+                                X2 = int(chunkTS['{}'.format('X2')][j])
+                                X3 = int(chunkTS['{}'.format('X3')][j])
+                                                
+                                if Y1 == Y3 and Y1==Y2 and X1 == X3 and X1 == X2:
+                                    B[0] = (X1*1000000+Y1*1000+1)
+                                    B[1] = 0
+                                    B[2] = 0
+                                elif Y3==Y2 and X3==X2:
+                                    B[0] = (X1*1000000+Y1*1000+1)
+                                    B[1] = (X2*1000000+Y2*1000+1)
+                                    B[2] = 0
+                                elif Y1==Y2 and X1==X2:
+                                    B[0] = (X2*1000000+Y2*1000+1)
+                                    B[1] = 0
+                                    B[2] = (X3*1000000+Y3*1000+1)
+                                elif Y1==Y3 and X1==X3:
+                                    B[0] = (X1*1000000+Y1*1000+1)
+                                    B[1] = (X2*1000000+Y2*1000+1)
+                                    B[2] = 0
+                                else:
+                                    B[0] = (X1*1000000+Y1*1000+1)
+                                    B[1] = (X2*1000000+Y2*1000+1)
+                                    B[2] = (X3*1000000+Y3*1000+1)
+                                for i in range(360):
+                                        D = chunkC['{}'.format(i)][j]
+                                        if pd.isna(D) == False and pd.isna(chunkE['{}'.format(i)][j])==False:
+                                                DD = int(D)
+                                                C = EcalID(DD).cell()
+                                                M = EcalID(DD).module()
+                                                L = EcalID(DD).layer()
+                                                B[i+3] = (L+1)*10000000+1000000*(M)+1000*C+chunkE['{}'.format(i)][j]
+                                B = np.array(B)
+                                np.savez_compressed(output_path + '3e{}.npz'.format(nc), B)
+                        else:
+                            for j in range(nc*NRR,(nc+1)*NRR):
+                                B = np.zeros((363))
+                                Y1 = int(chunkTS['{}'.format('Y1')][j])
+                                Y2 = int(chunkTS['{}'.format('Y2')][j])
+                                Y3 = int(chunkTS['{}'.format('Y3')][j])
+                                                
+                                X1 = int(chunkTS['{}'.format('X1')][j])
+                                X2 = int(chunkTS['{}'.format('X2')][j])
+                                X3 = int(chunkTS['{}'.format('X3')][j])
+                                                
+                                if Y1 == Y3 and Y1==Y2 and X1 == X3 and X1 == X2:
+                                    B[0] = (X1*1000000+Y1*1000+1)
+                                    B[1] = 0
+                                    B[2] = 0
+                                elif Y3==Y2 and X3==X2:
+                                    B[0] = (X1*1000000+Y1*1000+1)
+                                    B[1] = (X2*1000000+Y2*1000+1)
+                                    B[2] = 0
+                                elif Y1==Y2 and X1==X2:
+                                    B[0] = (X2*1000000+Y2*1000+1)
+                                    B[1] = 0
+                                    B[2] = (X3*1000000+Y3*1000+1)
+                                elif Y1==Y3 and X1==X3:
+                                    B[0] = (X1*1000000+Y1*1000+1)
+                                    B[1] = (X2*1000000+Y2*1000+1)
+                                    B[2] = 0
+                                else:
+                                    B[0] = (X1*1000000+Y1*1000+1)
+                                    B[1] = (X2*1000000+Y2*1000+1)
+                                    B[2] = (X3*1000000+Y3*1000+1)
+                                for i in range(360):
+                                        D = chunkC['{}'.format(i)][j]
+                                        if pd.isna(D) == False and pd.isna(chunkE['{}'.format(i)][j])==False:
+                                                DD = int(D)
+                                                C = EcalID(DD).cell()
+                                                M = EcalID(DD).module()
+                                                L = EcalID(DD).layer()
+                                                B[i+3] = (L+1)*10000000+1000000*(M)+1000*C+chunkE['{}'.format(i)][j]
+                                B = np.array(B)
+                                np.savez_compressed(output_path + '3e{}.npz'.format(nc), B)
+                        nTS = nTS+1
+                nE = nE+1
+        nc = nc+1
